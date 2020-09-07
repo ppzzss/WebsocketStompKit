81437a289aff0727b0f2a5a4e04e638e07d0ffd4
PZS <pzs.buaa@gmail.com>
2020-09-07 22:43:01 +0800

    Fix the bug that can't send Chinese Characters
    Fix the bug that can't send Chinese Characters

--- WebsocketStompKit/WebsocketStompKit.m
+++ WebsocketStompKit/WebsocketStompKit.m
@@ -364,7 +364,7 @@ CFAbsoluteTime serverActivity;
     NSMutableDictionary *msgHeaders = [NSMutableDictionary dictionaryWithDictionary:headers];
     msgHeaders[kHeaderDestination] = destination;
     if (body) {
-        msgHeaders[kHeaderContentLength] = [NSNumber numberWithLong:[body length]];
+        msgHeaders[kHeaderContentLength] = [NSNumber numberWithLong:[[body dataUsingEncoding: NSUTF8StringEncoding]  length]];
     }
     [self sendFrameWithCommand:kCommandSend
                        headers:msgHeaders
